bd81d16dc881559d3fddc948d644520e46c641e1
Nikita <emper137137@gmail.com>
2022-04-20 10:22:00 +1000

    redis microservice setup start

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'REDIS_SERVICE',
+        transport: Transport.REDIS,
+        options: { url: 'redis://localhost:6379' },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
