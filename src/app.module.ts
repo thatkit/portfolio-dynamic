cb6d964c67d342bc3430073a6412f18986a70152
Nikita <emper137137@gmail.com>
2022-04-22 17:05:18 +1000

    before GQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisCacheModule } from './redis-cache/redis-cache.module';
 import { GithubApiModule } from './github-api/github-api.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
     imports: [
-        GraphQLModule.forRoot<ApolloDriverConfig>({
-            driver: ApolloDriver,
-            typePaths: ['./**/*.graphql'],
-            definitions: {
-                path: join(process.cwd(), 'src/graphql.ts'),
-                outputAs: 'class'
-            }
-        }),
         ClientsModule.register([
             {
                 name: 'REDIS_SERVICE',
