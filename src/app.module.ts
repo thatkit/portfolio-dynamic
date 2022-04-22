be92525e53a0d876dd30c31a3bd29923e0e13646
Nikita <emper137137@gmail.com>
2022-04-22 11:26:57 +1000

    need to follow nestjs migration guide once again

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisCacheModule } from './redis-cache/redis-cache.module';
 import { GithubApiModule } from './github-api/github-api.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
     imports: [
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            typePaths: ['./**/*.graphql'],
+            definitions: {
+                path: join(process.cwd(), 'src/graphql.ts'),
+                outputAs: 'class'
+            }
+        }),
         ClientsModule.register([
             {
                 name: 'REDIS_SERVICE',
--- src/githubClient/index.ts
+++ src/githubClient/index.ts
@@ -1,5 +1,32 @@
+/* eslint-disable prettier/prettier */
 import { graphql } from '@octokit/graphql';
+// import type { RepositoryConnection } from './schema.docs.graphql';
 
-const { repos } = graphql(`
-  repos(username: "thatkit")
-`);
+class GithubClient {
+  userRepos;
+
+  get getUser() {
+    this.userRepos = async () => await graphql(
+      `
+        {
+          user(login: "thatkit") {
+            repositories(first: 100) {
+              nodes {
+                name
+                repositoryTopics(first: 100) {
+                  nodes {
+                    topic {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `
+    );
+    return this.userRepos;
+  }
+
+} 
