1ac8572256fc1b94fe6e881ec88b676f806ed0bb
Nikita <emper137137@gmail.com>
2022-04-25 08:31:56 +1000

    basic animation added to TagBtn

--- client/src/components/TagButton/index.tsx
+++ client/src/components/TagButton/index.tsx
@@ -1,5 +1,5 @@
 import { TagButtonProps } from '../App/types';
-import styles from './styles.module.css';
+import styles from './styles.module.scss';
 
 export const TagButton = ({ topic }: TagButtonProps): JSX.Element => {
   return (
